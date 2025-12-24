6dcf26a38b64b5de6b401750d7d6f06401bb696c
Karem Saeed <95093124+karemsaeed20020@users.noreply.github.com>
2025-12-24 09:53:17 +0200

    Add Response and ResponseHandler in project

---
+++ Sportwear.Core/Resources/SharedResources.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sportwear.Core.Resources
+{
+    public class SharedResources
+    {
+    }
+}
---
+++ Sportwear.Core/Resources/SharedResourcesKeys.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sportwear.Core.Resources
+{
+    public class SharedResourcesKeys
+    {
+        public const string Required = "Required";
+        public const string NotFound = "NotFound";
+        public const string Deleted = "Deleted";
+        public const string Created = "Created";
+        public const string Success = "Success";
+        public const string NotEmpty = "NotEmpty";
+        public const string Updated = "Updated";
+        public const string UnAuthorized = "UnAuthorized";
+        public const string BadRequest = "BadRequest";
+        public const string UnprocessableEntity = "UnprocessableEntity";
+    }
+}
